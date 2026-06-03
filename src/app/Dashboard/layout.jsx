16cccc7c39a92d5e19e7bcbf5e6a543c309707ab
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-06-04 01:52:19 +0600

    dashboard layout configuration done

---
+++ src/app/Dashboard/layout.jsx
@@ -0,0 +1,15 @@
+import { SideBar } from '@/Components/DashBoardComponents/SideBar/SideBar';
+import React from 'react';
+
+const DashBoardlayout = ({children}) => {
+    return (
+        <div className='mt-20 flex '>
+            <SideBar/>
+            <div className='flex-1'>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default DashBoardlayout;
